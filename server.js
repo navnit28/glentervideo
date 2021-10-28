4c0d34d26e3bbb38b5ee45a8b1f620db6d50d7ab
Navnit Anand <54511815+navnit28@users.noreply.github.com>
2021-10-28 22:47:17 +0530

    myvideo available

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express=require('express');
 const app=express();
 const server =require('http').Server(app);
-app.set('view engine','ejs')
 const {v4: uuidv4}=require('uuid');
-
+app.set('view engine','ejs')
+app.use(express.static('public'))
 
 
 app.get('/',(req,res)=>{
