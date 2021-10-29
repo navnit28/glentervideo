5f63b83452a707e2765218b3f252376024594aef
Navnit Anand <54511815+navnit28@users.noreply.github.com>
2021-10-29 23:24:00 +0530

    user added done

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const server =require('http').Server(app);
+const io=require('socket.io')(server)
 const {v4: uuidv4}=require('uuid');
 app.set('view engine','ejs')
 app.use(express.static('public'))
@@ -14,4 +15,11 @@ app.get('/:room',(req,res)=>{
     res.render('room',{roomId: req.params.room})
 })
 
+io.on('connection', socket => {
+    socket.on('join-room', (roomId) =>{
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected');
+
+    })
+})
 server.listen(3030);
